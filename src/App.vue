865f73d093f7a22b6743bb611ab0b5f5dd8efe20
ChoHyeonSeok <hs.cho9647@gmail.com>
2023-01-11 19:08:16 +0900

    clickEvent
    함수사용법 추가
    이벤트 핸들링

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,8 @@
   <div v-for="value in products" :key="value.product">
     <h4 :style="style">{{ value.product }}</h4>
     <p>{{ value.price }} 만원</p>
+    <button @click="increase(value.product)">신고하기</button>
+    <span>신고수 : {{ counts[value.product] }}</span>
   </div>
 </template>
 
@@ -14,6 +16,11 @@ export default {
   name: "App",
   data() {
     return {
+      counts: {
+        역삼동원룸: 0,
+        천호동원룸: 0,
+        마포구원룸: 0,
+      },
       menus: ["Home", "Products", "About"],
       products: [
         { product: "역삼동원룸", price: 50 },
@@ -22,6 +29,11 @@ export default {
       ],
       style: "color: blue",
     };
+  },
+  methods: {
+    increase(key) {
+      this.counts[key] += 1;
+    },
   },
   components: {},
 };
