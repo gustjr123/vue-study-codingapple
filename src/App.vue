09bfe96f815abd9a70bb09fca52d75b9b060a14e
ChoHyeonSeok <hs.cho9647@gmail.com>
2023-01-12 21:27:09 +0900

    modal창 추가
    v-if사용법
    v-if: "조건문"
    조건문이 true면 해당 구문 보여줌

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,28 @@
 <template>
+  <div class="black-bg" v-if="isModalOpen">
+    <div class="white-bg">
+      <div class="bg-close-botten">
+        <button
+          @click="
+            () => {
+              isModalOpen = false;
+            }
+          "
+        >
+          X
+        </button>
+      </div>
+      <h4>상세페이지</h4>
+      <p>상세페이지 내용</p>
+    </div>
+  </div>
+
   <div class="menu">
     <a v-for="i in menus" :key="i"> {{ i }} </a>
   </div>
 
   <div v-for="value in products" :key="value.product">
-    <h4 :style="style">{{ value.product }}</h4>
+    <h4 @click="isModalOpen = true" :style="style">{{ value.product }}</h4>
     <p>{{ value.price }} 만원</p>
     <button @click="increase(value.product)">신고하기</button>
     <span>신고수 : {{ counts[value.product] }}</span>
@@ -12,10 +30,14 @@
 </template>
 
 <script>
+// 동적인 UI 만드는 법칙
+// 1. UI의 현재 상태를 데이터로 저장해둠 -> isModalOpen
+// 2. 데이터에 따라 UI가 어떻게보일지 작성 -> isModalOpen에 따라 보여주고 안보여주고
 export default {
   name: "App",
   data() {
     return {
+      isModalOpen: false,
       counts: {
         역삼동원룸: 0,
         천호동원룸: 0,
@@ -40,6 +62,31 @@ export default {
 </script>
 
 <style>
+body {
+  margin: 100;
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  padding: 20px;
+  background: rgba(0, 0, 0, 0.5);
+}
+.white-bg {
+  width: 60%;
+  background: white;
+  padding: 20px;
+  border-radius: 8px;
+  margin: auto;
+}
+.bg-close-botten {
+  width: fit-content;
+  margin-left: 95%;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
